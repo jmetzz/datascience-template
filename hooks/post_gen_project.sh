07a850f25d0267d151f33bcf01e299cc6b0dcac0
Jean Metz <jmetzz@gmail.com>
2024-09-26 14:54:38 +0200

    feat(hooks): add pre and post hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-make deps
-make lint
+# Define color codes
+GREEN = "\033[0;32m"
+CYAN = "\033[0;36m"
+YELLOW = "\033[0;33m"
+NC = "\033[0m"  # No Color
+FIRE = "\U0001F525"  # 🔥
+
+# Replace <your-project-directory> with the actual directory name
+PROJECT_DIR = "{{ cookiecutter.project_slug }}"
+
+# Print messages with colors
+print(f"{GREEN}All set!{NC}")
+print(f"{GREEN}Enter in your new project directory, install the dependencies and create the first commit:{NC}")
+print(f"{GREEN}> {YELLOW}cd {PROJECT_DIR}{NC}")
+print(f"{GREEN}> {YELLOW}make deps license{NC}")
+print(f"{GREEN}> {YELLOW}make lint test{NC}")
+print(f"{GREEN}> {YELLOW}git add .{NC}")
+print(f"{GREEN}> {YELLOW}git commit -m \"Initial commit\"{NC}")
+print()
+print(f"{CYAN}{FIRE} TIP:{YELLOW} check out the 'make help' command{NC}")
+print(f"{GREEN}Enjoy working on your new project!{NC}")
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,13 +2,27 @@ import re
 import sys
 
 
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+def validate_module_slug(module_slug):
+    regex = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+    if not re.match(regex, module_slug):
+        print(  # noqa: T201
+            f"ERROR: The project slug ({module_slug}) is not a valid Python module name. "
+            "Please do not use a - and use _ instead"
+        )
+        sys.exit(1)
 
-module_name = "{{cookiecutter.project_slug}}"
 
-if not re.match(MODULE_REGEX, module_name):
-    print(  # noqa: T201
-        f"ERROR: The project slug ({module_name}) is not a valid Python module name. "
-        "Please do not use a - and use _ instead"
-    )
-    sys.exit(1)
+def validate_project_slug(project_slug):
+    regex = r"^[a-zA-Z][-a-zA-Z0-9]+$"
+    if not re.match(regex, project_slug):
+        print(f"Invalid project_slug '{project_slug}'. It must match the regex: {regex}")
+        sys.exit(1)
+
+
+def main():
+    validate_project_slug("{{ cookiecutter.project_slug }}")
+    validate_module_slug("{{cookiecutter.__module_slug}}")
+
+
+if __name__ == "__main__":
+    main()
